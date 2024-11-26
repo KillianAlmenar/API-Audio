1361d54c9fc4e8ed4a655282c89b3c886599c56d
Swiiner <almenar.killian@hotmail.com>
2024-11-26 15:53:51 +0100

    Fix ButtonName

--- API Audio/API Audio/APIwindow.h
+++ API Audio/API Audio/APIwindow.h
@@ -37,11 +37,14 @@ namespace APIAudio {
 			}
 		}
 	private: System::Windows::Forms::Button^ PlayBtn;
+	private: System::Windows::Forms::Button^ PauseBtn;
+	private: System::Windows::Forms::Button^ StopBtn;
 	protected:
 
-	private: System::Windows::Forms::Button^ Pause;
-	private: System::Windows::Forms::Button^ Stop;
-	private: System::Windows::Forms::Button^ Resume;
+
+
+	private: System::Windows::Forms::Button^ ResumeBtn;
+
 	protected:
 
 	protected:
@@ -60,9 +63,9 @@ namespace APIAudio {
 		void InitializeComponent(void)
 		{
 			this->PlayBtn = (gcnew System::Windows::Forms::Button());
-			this->Pause = (gcnew System::Windows::Forms::Button());
-			this->Stop = (gcnew System::Windows::Forms::Button());
-			this->Resume = (gcnew System::Windows::Forms::Button());
+			this->PauseBtn = (gcnew System::Windows::Forms::Button());
+			this->StopBtn = (gcnew System::Windows::Forms::Button());
+			this->ResumeBtn = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// PlayBtn
@@ -75,41 +78,42 @@ namespace APIAudio {
 			this->PlayBtn->UseVisualStyleBackColor = true;
 			this->PlayBtn->Click += gcnew System::EventHandler(this, &APIwindow::Play_Click);
 			// 
-			// Pause
+			// PauseBtn
 			// 
-			this->Pause->Location = System::Drawing::Point(219, 140);
-			this->Pause->Name = L"Pause";
-			this->Pause->Size = System::Drawing::Size(75, 23);
-			this->Pause->TabIndex = 1;
-			this->Pause->Text = L"Pause";
-			this->Pause->UseVisualStyleBackColor = true;
+			this->PauseBtn->Location = System::Drawing::Point(219, 140);
+			this->PauseBtn->Name = L"PauseBtn";
+			this->PauseBtn->Size = System::Drawing::Size(75, 23);
+			this->PauseBtn->TabIndex = 1;
+			this->PauseBtn->Text = L"Pause";
+			this->PauseBtn->UseVisualStyleBackColor = true;
 			// 
-			// Stop
+			// StopBtn
 			// 
-			this->Stop->Location = System::Drawing::Point(343, 213);
-			this->Stop->Name = L"Stop";
-			this->Stop->Size = System::Drawing::Size(75, 23);
-			this->Stop->TabIndex = 2;
-			this->Stop->Text = L"Stop";
-			this->Stop->UseVisualStyleBackColor = true;
+			this->StopBtn->Location = System::Drawing::Point(343, 213);
+			this->StopBtn->Name = L"StopBtn";
+			this->StopBtn->Size = System::Drawing::Size(75, 23);
+			this->StopBtn->TabIndex = 2;
+			this->StopBtn->Text = L"Stop";
+			this->StopBtn->UseVisualStyleBackColor = true;
+			this->StopBtn->Click += gcnew System::EventHandler(this, &APIwindow::Stop_Click);
 			// 
-			// Resume
+			// ResumeBtn
 			// 
-			this->Resume->Location = System::Drawing::Point(343, 140);
-			this->Resume->Name = L"Resume";
-			this->Resume->Size = System::Drawing::Size(75, 23);
-			this->Resume->TabIndex = 3;
-			this->Resume->Text = L"Resume";
-			this->Resume->UseVisualStyleBackColor = true;
+			this->ResumeBtn->Location = System::Drawing::Point(343, 140);
+			this->ResumeBtn->Name = L"ResumeBtn";
+			this->ResumeBtn->Size = System::Drawing::Size(75, 23);
+			this->ResumeBtn->TabIndex = 3;
+			this->ResumeBtn->Text = L"Resume";
+			this->ResumeBtn->UseVisualStyleBackColor = true;
 			// 
 			// APIwindow
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(512, 303);
-			this->Controls->Add(this->Resume);
-			this->Controls->Add(this->Stop);
-			this->Controls->Add(this->Pause);
+			this->Controls->Add(this->ResumeBtn);
+			this->Controls->Add(this->StopBtn);
+			this->Controls->Add(this->PauseBtn);
 			this->Controls->Add(this->PlayBtn);
 			this->Name = L"APIwindow";
 			this->Text = L"APIwindow";
@@ -121,5 +125,11 @@ namespace APIAudio {
 	{
 		Play(MUSIC_01);
 	}
-	};
+
+	private: System::Void Stop_Click(System::Object^ sender, System::EventArgs^ e) 
+	{
+
+	}
+
+};
 }
