95f141b762d4c4899057b7afa17afcc836d92249
Luc D'eurveilher <eugened974@gmail.com>
2024-12-03 15:27:53 +0100

    Update APIwindow.h

--- API Audio/API Audio/APIwindow.h
+++ API Audio/API Audio/APIwindow.h
@@ -203,7 +203,7 @@ namespace APIAudio {
 			this->Controls->Add(this->PauseBtn);
 			this->Controls->Add(this->PlayBtn);
 			this->Name = L"APIwindow";
-			this->Text = L"API OpenAl";
+			this->Text = L"API OpenAL";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->VolumeBar))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
